7081914e52cf50322f75617aa49f8ad1dd840813
al3sc <alesc.alfarano@gmail.com>
2025-01-15 16:06:36 +0100

    pages and components rollback, upload/download-artifact update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import ImageForm from '../components/ImageForm';
-import showedImage from '../files/download_image.jpg';
+import DownloadForm from '../components/DownloadForm';
+import downloadImage from '../files/download_image.jpg';
 
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
@@ -10,10 +10,9 @@ function Home() {
   return(
     <>
       <h2>Home Page</h2>
-      
-      <Button onClick={() => navigate("/react-page/test/test_param")}>to Test Page ...</Button>
-      
-      <ImageForm showedImage={showedImage} />
+      <DownloadForm downloadImage={downloadImage} />
+
+      <Button onClick={() => navigate("/react-page/test/test_param")}>to Test Page</Button>
     </>
   )
 }
